13b74229cb71040d86da8a686fc77f821d4aaa85
Olly Warren <bunoire14@icloud.com>
2017-10-02 23:04:19 +0100

    Circumvented the ENVRIONMENT CLASS CONSTANT

--- src/services/GoCardlessService.php
+++ src/services/GoCardlessService.php
@@ -3,39 +3,38 @@
 namespace Ollywarren\LaravelGoCardless\Services;
 
 use GoCardlessPro\Client;
-use GoCardlessPro\Environment;
 use BadMethodCallException;
 
 class GoCardlessService
 {
-    private $environment;
-    private $token;
-    private $client;
+	private $environment;
+	private $token;
+	private $client;
 
-    public function __construct()
-    {
-        $this->token        = config('gocardless.token');
-        $this->environment  = config('gocardless.environment');
+	public function __construct()
+	{
+		$this->token        = config('gocardless.token');
+		$this->environment  = config('gocardless.environment');
 
-        $this->client = new Client( array (
-          'access_token' => $this->token,
-          'environment'  => constant("Environment::".$this->environment),
-        ));
-    }
+		$this->client = new Client( array (
+			'access_token' => $this->token,
+			'environment'  => $this->environment
+		));
+	}
 
-    public function __call($method, $args) {
-        if( is_callable([$this->client, $method]) ) {
-            return call_user_func_array([$this->client, $method],$args);
-        } else {
-            throw new BadMethodCallException("Method $method does not exist on Go Cardless Library");
-        }
-    }
+	public function __call($method, $args) {
+		if( is_callable([$this->client, $method]) ) {
+			return call_user_func_array([$this->client, $method],$args);
+		} else {
+			throw new BadMethodCallException("Method $method does not exist on Go Cardless Library");
+		}
+	}
 
-    public function __get($property) {
-        if( property_exists($this->client, $property) ) {
-            return $this->client->{$property};
-        } else {
-            throw new BadMethodCallException("Property $property does not exist on Go Cardless Library");
-        }
-    }
+	public function __get($property) {
+		if( property_exists($this->client, $property) ) {
+			return $this->client->{$property};
+		} else {
+			throw new BadMethodCallException("Property $property does not exist on Go Cardless Library");
+		}
+	}
 }
